f71b4395bdc1ee7138082e777c7eb19adfb1ccba
Plozel <51715911+Plozel@users.noreply.github.com>
2020-09-29 15:44:58 +0300

    with crf inference

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import yaml
 import csv
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -47,9 +48,9 @@ def test_all_separate(_class_1_classifier, _class_2_classifier, _class_3_classif
     _class_3_classifier.run_test()
 
 
-def test_all_together(_class_1_classifier, _class_2_classifier, _class_3_classifier):
+def crf_inference(_class_1_classifier, _class_2_classifier, _class_3_classifier):
     print("-----------------------")
-    print("Test all models:")
+    print("Test all models through crf inference:")
     print("-----------------------")
 
     test_loader = _class_1_classifier.test_loader
@@ -61,7 +62,13 @@ def test_all_together(_class_1_classifier, _class_2_classifier, _class_3_classif
 
     i = 0
     num_of_correct = [0, 0, 0]
+    # num_of_correct = 0
     total = 0
+
+    # for i in range(3):
+    #     with open('../TrainedModels/class_{}_predictions.csv'.format(i), 'w') as f:
+    #         pass
+
     for test_batch in tqdm(test_loader):
         with torch.no_grad():
             stocks_id, sml, features, true_labels_1, true_labels_2, true_labels_3 = test_batch[0].to(device), \
@@ -75,24 +82,37 @@ def test_all_together(_class_1_classifier, _class_2_classifier, _class_3_classif
                        _class_2_classifier.model(stocks_id, sml, features).to(device),
                        _class_3_classifier.model(stocks_id, sml, features).to(device)]
 
-            for i in range(3):
-                with open('../TrainedModels/class_{}_predictions.csv'.format(i), 'a') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([*outputs[i][0].tolist()])
+            # in case we want to export the predictions into a csv file:
+            # for i in range(3):
+            #     with open('../TrainedModels/class_{}_predictions.csv'.format(i), 'a') as f:
+            #         writer = csv.writer(f)
+            #         writer.writerow([*outputs[i][0].tolist()])
+
+            binary_mul_tmp = torch.einsum('ai, aj -> aij', outputs[0], outputs[1]).flatten(start_dim=1)
+            crf_outputs = binary_mul_tmp * outputs[2]
+
+            crf_outputs_1 = torch.cat(
+                [crf_outputs[:, 0:3].sum(dim=1).unsqueeze(1), crf_outputs[:, 3:6].sum(dim=1).unsqueeze(1),
+                 crf_outputs[:, 6:9].sum(dim=1).unsqueeze(1)], dim=1)
+            crf_outputs_2 = torch.cat(
+                [crf_outputs[:, np.r_[0, 3, 6]].sum(dim=1).unsqueeze(1),
+                 crf_outputs[:, np.r_[1, 4, 7]].sum(dim=1).unsqueeze(1),
+                 crf_outputs[:, np.r_[2, 5, 8]].sum(dim=1).unsqueeze(1)], dim=1)
 
             # each element in probs_and_preds is a tuple of maximum probability and
             # the prediction for a certain class- [(probs, predicted),...]
-            probs_and_preds = [torch.max(outputs[0].data, 1),
-                               torch.max(outputs[1].data, 1),
-                               torch.max(outputs[2].data, 1)]
+            probs_and_preds = [torch.max(crf_outputs_1.data, 1),
+                               torch.max(crf_outputs_2.data, 1),
+                               torch.max(crf_outputs.data, 1)]
 
             num_of_correct[0] += (probs_and_preds[0][1] == true_labels_1).sum()
             num_of_correct[1] += (probs_and_preds[1][1] == true_labels_2).sum()
             num_of_correct[2] += (probs_and_preds[2][1] == true_labels_3).sum()
+
             total += len(true_labels_1)
 
     for i in range(3):
-        print("class_{} ACC:".format(i))
+        print("class_{} ACC:".format(i+1))
         print(num_of_correct[i].item()/total)
 
 
@@ -115,4 +135,4 @@ if __name__ == '__main__':
 
     load_all(class_1_classifier, class_2_classifier, class_3_classifier)
     #
-    test_all_together(class_1_classifier, class_2_classifier, class_3_classifier)
+    crf_inference(class_1_classifier, class_2_classifier, class_3_classifier)
